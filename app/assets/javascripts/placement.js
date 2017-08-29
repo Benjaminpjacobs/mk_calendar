a127483a31eea0f062b63f984b17d15385f13aa6
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-29 16:33:47 -0600

    adds some color and input styling to input

--- app/assets/javascripts/placement.js
+++ app/assets/javascripts/placement.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
                 const id = event.id
                 const startTime = event.start_time
                 const length = event.length
-                debugger
                 const th = `<th rowspan=${length}>Event ${id}</th>`
                 if (startTime.substr(2, 3) === '30') {
                     $(th).insertAfter(`#${startTime}`)
@@ -14,5 +13,4 @@ $(document).ready(function() {
                 }
             });
         })
-
 })
