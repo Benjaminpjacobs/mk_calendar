21115c477bb40d64e6cd444e2bf9be939aee041a
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-29 16:52:14 -0600

    creates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Event.destroy_all
+Event.create!([{start_time: '09:00', end_time: '10:00'}, 
+               {start_time: '10:00', end_time: '11:00'},
+               {start_time: '10:30', end_time: '15:00'},
+               {start_time: '11:00', end_time: '11:00'},
+               {start_time: '11:30', end_time: '14:00'},
+               {start_time: '13:00', end_time: '14:00'},
+               {start_time: '15:00', end_time: '18:00'},])
