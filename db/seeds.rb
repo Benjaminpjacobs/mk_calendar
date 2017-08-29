25146c0284461ad6e51ebdb24dc556f6dc2e9e59
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-29 16:55:12 -0600

    clean seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
-
 Event.destroy_all
 Event.create!([{start_time: '09:00', end_time: '10:00'}, 
                {start_time: '10:00', end_time: '11:00'},
