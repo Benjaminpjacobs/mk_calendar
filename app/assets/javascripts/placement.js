f135b36610378c9c603bf28f3ebccf0ae06d1b40
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-08-29 15:55:53 -0600

    creates JS function to ping api and populate very rough event objects to view

--- app/assets/javascripts/placement.js
+++ app/assets/javascripts/placement.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
     $.getJSON('http://localhost:3000/api/v1/events')
         .then(function(data) {
-            debugger
+            data.forEach(function(event) {
+                const id = event.id
+                const startTime = event.start_time
+                const length = (Number(event.end_time) - Number(startTime)) / 100
+
+                const th = `<th rowspan=${length}>Event ${id}</th>`
+                debugger
+                $(`#${startTime}`).append(th)
+            });
         })
 
 })
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,11 +2,10 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :start_time, :end_time
 
   def start_time
-    binding.pry
-    object.start_time.strftime('%H:%M')
+    object.start_time.strftime('%H%M')
   end
   
   def end_time
-    object.start_time.strftime('%H:%M')
+    object.end_time.strftime('%H%M')
   end
 end
